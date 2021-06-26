9d88b9aa91b613cfa9530ca233de7d81fad6c80b
Amandeep Kaur <46717101+ak53@users.noreply.github.com>
2021-06-26 12:49:25 +0530

    Delete project directory

--- project/src/dt_gpu.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#ifndef _DT_GPU_H_
-#define _DT_GPU_H_
-
-void computeDT_GPU(std::vector<std::vector<Point*> > shapePoints, std::vector<float*> shapeBounds, std::vector<std::vector<int> > triangles);
-
-#endif
